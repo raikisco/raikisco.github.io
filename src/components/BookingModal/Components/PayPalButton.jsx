946970a7191cc7ee0bb809276be027a6d17928b3
tobias Gu <tobias.goo@gmail.com>
2018-03-02 19:00:16 -0800

    new build

--- src/components/BookingModal/Components/PayPalButton.jsx
+++ src/components/BookingModal/Components/PayPalButton.jsx
@@ -1,44 +1,56 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-export default function PayPalButton(props) {
-  let { price, desc } = props;
-  console.log("price is", price);
-  let client = {
-    sandbox:
-      "AcvFd-dhb4tHQ8wNAhpWMa7vynAbgkiFqyxlsNwNzi3bb8EWvHI8XZSO7srZRtPl5ewFHlVHkhGUniIZ",
-    production:
-      "AZN4VoCjvnf6XGMcK-kZ1g3byOD7Gg5eErOCwSxD_7t_lu3giMTS_F99_GUbWya-boOv05INyKqCXWH_"
-  };
-  let payment = (data, actions) => {
-    // Make a call to the REST api to create the payment
-    return actions.payment.create({
-      payment: {
-        transactions: [
-          {
-            amount: { total: price, currency: "CAD" }
-          }
-        ]
-      }
-    });
-  };
+export default class PayPalButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  componentDidMount() {
+    this.setState({ clientSide: true });
+  }
+  render() {
+    let props = this.props;
+    let { price, desc } = props;
+    let client = {
+      sandbox:
+        "AcvFd-dhb4tHQ8wNAhpWMa7vynAbgkiFqyxlsNwNzi3bb8EWvHI8XZSO7srZRtPl5ewFHlVHkhGUniIZ",
+      production:
+        "AZN4VoCjvnf6XGMcK-kZ1g3byOD7Gg5eErOCwSxD_7t_lu3giMTS_F99_GUbWya-boOv05INyKqCXWH_"
+    };
+    let payment = (data, actions) => {
+      // Make a call to the REST api to create the payment
+      return actions.payment.create({
+        payment: {
+          transactions: [
+            {
+              amount: { total: price, currency: "CAD" }
+            }
+          ]
+        }
+      });
+    };
 
-  let onAuthorize = (data, actions) => {
-    return actions.payment.execute().then(function() {
-      props.onPaymentComplete();
-      window.alert("Payment Complete - We've reserved your spot!");
-    });
-  };
-  let PayPalButton = paypal.Button.driver("react", { React, ReactDOM });
-  return (
-    <div className="shoppingCart">
-      <PayPalButton
-        env="sandbox" //sandbox || production
-        client={client}
-        payment={payment}
-        commit={true}
-        onAuthorize={onAuthorize}
-      />
-    </div>
-  );
+    let onAuthorize = (data, actions) => {
+      return actions.payment.execute().then(function() {
+        props.onPaymentComplete();
+        window.alert("Payment Complete - We've reserved your spot!");
+      });
+    };
+    if (this.state.clientSide){
+      let PayPalButton = paypal.Button.driver("react", { React, ReactDOM });
+      return (
+        <div className="shoppingCart">
+          <PayPalButton
+            env="sandbox" //sandbox || production
+            client={client}
+            payment={payment}
+            commit={true}
+            onAuthorize={onAuthorize}
+          />
+        </div>
+      );
+    }
+    return null
+  }
 }
